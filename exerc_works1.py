6f6c8334f763e7f5bc555b14a415a7d8a3a2aed0
Xavier-7ca <manassesp.b@gmail.com>
2023-08-28 17:04:13 -0300

    Atividade do 01 dia

---
+++ exerc_works1.py
@@ -0,0 +1,4 @@
+numero = int(input("Digite um número e veja seu sucessor e seu antecessor: "))
+suce = numero + 1
+ante = numero - 1
+print("O número que você digitou foi: {} e o seu antecessor é: {} e o sucessor é: {}".format(numero, ante, suce))
